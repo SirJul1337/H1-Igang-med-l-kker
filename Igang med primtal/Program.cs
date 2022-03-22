6116b290dc04b5269079f801e8364cc238206fc5
nj <niklas@julit.dk>
2022-03-22 09:36:50 +0100

    Add project files.

---
+++ Igang med primtal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Igang_med_primtal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("While loop\n");
+            while (i <100)
+            {
+                if (i < 50)
+                {
+                    Console.WriteLine(i);
+                    
+
+                }
+                i++;
+
+            }
+            Console.WriteLine("\nFor Loop \n");
+            for (int j = 100; j > 0; j--)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
